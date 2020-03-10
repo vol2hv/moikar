a218ff7063198afe0ef5bf13f8e2bb3019d3c9b1
vol2hv <vol2hv@yandex.ru>
2020-03-10 11:26:19 +0300

    Аутенфикация базовая

---
+++ src/main/java/com/madv/moikar/config/EncriptionConfig.java
@@ -0,0 +1,19 @@
+package com.madv.moikar.config;
+
+/*
+ * Bean для настройки кодировцика паролей
+ * */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncriptionConfig {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+}
+
--- src/main/java/com/madv/moikar/config/WebSecurityConfig.java
+++ src/main/java/com/madv/moikar/config/WebSecurityConfig.java
@@ -2,30 +2,70 @@ package com.madv.moikar.config;
 
 import com.madv.moikar.domain.User;
 import com.madv.moikar.repo.UserDetailsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.time.LocalDateTime;
 
 @Configuration
 @EnableWebSecurity
 @EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    // Create 2 users for demo
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                    .password(passwordEncoder.encode("111")).roles("USER")
+                .and()
+                .withUser("admin")
+                    .password(passwordEncoder.encode("111")).roles("USER", "ADMIN");
+
+    }
+
+    // Secure the endpoins with HTTP Basic authentication
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
+                .httpBasic()
+                .and()
                 .antMatcher("/**")
                 .authorizeRequests()
+//                .antMatchers("/api/**").permitAll()
+//                .anyRequest().authenticated()
                 .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
                 .anyRequest().authenticated()
                 .and().logout().logoutSuccessUrl("/").permitAll()
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                .formLogin().disable();
     }
+
+
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .antMatcher("/**")
+//                .authorizeRequests()
+//                .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
+//                .anyRequest().authenticated()
+//                .and().logout().logoutSuccessUrl("/").permitAll()
+//                .and()
+//                .csrf().disable();
+//    }
 
     @Bean
     public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
