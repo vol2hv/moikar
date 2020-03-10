5480505efba7c55ccb636d349ed1ee6ec87de1ac
vol2hv <vol2hv@yandex.ru>
2020-03-10 11:16:57 +0300

    Change BoxRepo.java. Аутенфикация по Auth2

--- src/main/java/com/madv/moikar/repo/BoxRepo.java
+++ src/main/java/com/madv/moikar/repo/BoxRepo.java
@@ -3,6 +3,8 @@ package com.madv.moikar.repo;
 import com.madv.moikar.domain.Box;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoxRepo extends JpaRepository<Box, Long> {
-    Box findByName(String name);
+    List<Box> findByName(String name);
 }
